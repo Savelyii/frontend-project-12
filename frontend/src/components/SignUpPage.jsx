10bfb9335971d9d764e9acde7a3f54e1bc136a4c
savelyii <leshaboichenko95@mail.ru>
2023-11-10 00:31:35 +0300

    update SignUpPage.jsx

--- frontend/src/components/SignUpPage.jsx
+++ frontend/src/components/SignUpPage.jsx
@@ -1,13 +1,156 @@
-import { Link } from 'react-router-dom';
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
+import { useFormik } from 'formik';
+import { useNavigate } from 'react-router-dom';
+import { Button, Form } from 'react-bootstrap';
+import * as yup from 'yup';
 
-const SignUpPage = () => (
-  <>
-    <h1>Зарегестрироваться</h1>
-    <p>
-      Уже есть аккаунт?
-      <Link to="/login">Войти</Link>
-    </p>
-  </>
-);
+import { useAuth } from '../hooks/index.js';
+import routes from '../routes.js';
+
+import image from '../assets/avatar_1.jpg';
+
+const SignUpPage = () => {
+  const auth = useAuth();
+  const [signUpFailed, setSignUpFailed] = useState(false);
+  const inputRef = useRef();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const validationSchema = yup.object().shape({
+    username: yup
+      .string()
+      .trim()
+      .required('Обязательное поле')
+      .min(3, 'От 3 до 20 символов')
+      .max(20, 'От 3 до 20 символов'),
+    password: yup
+      .string()
+      .trim()
+      .required('Обязательное поле')
+      .min(6, 'Не менее 6 символов'),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref('password')], 'Пароли должны совпадать'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema,
+    onSubmit: async (values) => {
+      setSignUpFailed(false);
+      try {
+        const response = await axios.post(
+          routes.signupPath(),
+          { username: values.username, password: values.password },
+        );
+        auth.logIn(response.data);
+        navigate('/');
+      } catch (err) {
+        if (err.response.status === 409) {
+          setSignUpFailed(true);
+          inputRef.current.select();
+        }
+        throw err;
+      }
+    },
+  });
+
+  return (
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12 col-md-8 col-xxl-6">
+          <div className="card shadow-sm">
+            <div className="card-body d-flex flex-column flex-md-row justify-content-around align-items-center p-5">
+              <div>
+                <img
+                  src={image}
+                  className="rounded-circle"
+                  alt="Регистрация"
+                />
+              </div>
+              <Form onSubmit={formik.handleSubmit} className="w-50">
+                <h1 className="text-center mb-4">Регистрация</h1>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Control
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.username}
+                    placeholder="От 3 до 20 символов"
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    isInvalid={
+                      (formik.errors.username && formik.touched.username)
+                      || signUpFailed
+                    }
+                    required
+                    ref={inputRef}
+                  />
+                  <Form.Label htmlFor="username">Имя пользователя</Form.Label>
+                  <Form.Control.Feedback type="invalid" tooltip placement="right">
+                    {formik.errors.username}
+                  </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="form-floating mb-3">
+                  <Form.Control
+                    type="password"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                    placeholder="Не менее 6 символов"
+                    name="password"
+                    id="password"
+                    aria-describedby="passwordHelpBlock"
+                    isInvalid={
+                      (formik.errors.password && formik.touched.password)
+                      || signUpFailed
+                    }
+                    required
+                    autoComplete="new-password"
+                  />
+                  <Form.Control.Feedback type="invalid" tooltip>
+                    {formik.errors.password}
+                  </Form.Control.Feedback>
+                  <Form.Label htmlFor="password">Пароль</Form.Label>
+                </Form.Group>
+                <Form.Group className="form-floating mb-4">
+                  <Form.Control
+                    type="password"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.confirmPassword}
+                    placeholder="Пароли должны совпадать"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    isInvalid={
+                      (formik.errors.confirmPassword && formik.touched.confirmPassword)
+                      || signUpFailed
+                    }
+                    required
+                    autoComplete="new-password"
+                  />
+                  <Form.Control.Feedback type="invalid" tooltip>
+                    {signUpFailed ? 'Такой пользователь уже существует' : formik.errors.confirmPassword}
+                  </Form.Control.Feedback>
+                  <Form.Label htmlFor="confirmPassword">Подтвердите пароль</Form.Label>
+
+                </Form.Group>
+                <Button type="submit" variant="outline-primary" className="w-100">Зарегистрироваться</Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SignUpPage;
