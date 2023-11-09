24fe66f89fdf48a2001dd4bf854838969487a958
savelyii <leshaboichenko95@mail.ru>
2023-11-10 02:07:57 +0300

    add image from NotFoundPage.jsx

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import image from '../assets/notFoundImage.svg';
 
 const NotFoundPage = () => (
   <div className="text-center">
+    <img alt="Страница не найдена" className="img-fluid h-25" src={image} />
     <h1 className="h4 text-muted">Страница не найдена</h1>
     <p className="text-muted">
       Но вы можете перейти
