6e3dafba96a8ef4658ff49b1bb7ecb6c706ac470
savelyii <leshaboichenko95@mail.ru>
2023-11-11 02:31:10 +0300

    fix rollbar

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -5,6 +5,7 @@ import {
   useDispatch,
   useSelector,
 } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
@@ -90,6 +91,13 @@ const SocketProvider = ({ socket, children }) => {
   );
 };
 
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR,
+  environment: 'production',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
+
 const init = async (socket) => {
   const i18n = i18next.createInstance();
 
@@ -101,13 +109,17 @@ const init = async (socket) => {
     });
 
   const vdom = (
-    <StoreProvider store={store}>
-      <SocketProvider socket={socket}>
-        <I18nextProvider i18n={i18n}>
-          <App />
-        </I18nextProvider>
-      </SocketProvider>
-    </StoreProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <StoreProvider store={store}>
+          <SocketProvider socket={socket}>
+            <I18nextProvider i18n={i18n}>
+              <App />
+            </I18nextProvider>
+          </SocketProvider>
+        </StoreProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 
   return vdom;
